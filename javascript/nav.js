7bb72d561fce22af71a5a026fa690b86c549c7ad
Vinod sharma <159933291+vinodsharma14@users.noreply.github.com>
2024-12-19 21:16:23 +0530

    Add files via upload

---
+++ javascript/nav.js
@@ -0,0 +1,12 @@
+<script scr="index.html">
+function myfuction(){
+var x=document.getElementById("menu");
+ if (x.style.display == "block"){
+     x.style.display = "none";
+ }
+ else{
+     x.style.display = "block"
+ }
+
+}
+</script>
